d10fd2016038d5a99092d0814729ce085860bcf7
Arnould Florian <arnould@ecole.ensicaen.fr>
2016-11-29 13:43:51 +0100

    add toolbar on ProjectActivity

--- app/src/main/java/com/socket/florian/todo/ProjectActivity.java
+++ app/src/main/java/com/socket/florian/todo/ProjectActivity.java
@@ -2,6 +2,7 @@ package com.socket.florian.todo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.widget.TextView;
 
 import com.socket.florian.todo.storage.Project;
@@ -13,8 +14,10 @@ public class ProjectActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tasks);
+        setContentView(R.layout.activity_project);
         _project = getIntent().getParcelableExtra("PROJECT");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle(_project.getName());
         TextView text = (TextView) findViewById(R.id.text);
         String str = _project.getName() + " : " + _project.getStartDate().toVerboseString();
         text.setText(str);
